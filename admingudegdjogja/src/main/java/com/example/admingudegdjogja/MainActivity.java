031fd63cdee1dfc06dd3337ff6cda9f565b72dd2
ikhsanramadhaan <ikhsanramadhan28@gmail.com>
2020-04-21 16:45:15 +0700

    add login

--- admingudegdjogja/src/main/java/com/example/admingudegdjogja/MainActivity.java
+++ admingudegdjogja/src/main/java/com/example/admingudegdjogja/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.admingudegdjogja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editTextUsername;
+    private EditText editTextPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button_login = findViewById(R.id.act_main_button_login);
+        editTextUsername = findViewById(R.id.act_main_edittext_username);
+        editTextPassword = findViewById(R.id.act_main_edittext_password);
+
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , HomeActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
